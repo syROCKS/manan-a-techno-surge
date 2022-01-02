7c2b409649edb3e2e838caf0df27963954ad6b2e
Arun Shekhar <arunshekhar001@gmail.com>
2022-01-03 01:41:55 +0530

    dev init

--- src/components/main/Landing/index.jsx
+++ src/components/main/Landing/index.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 const Landing = (props)=>{
     return (
       <React.Fragment>
-        <h1>Landing Page</h1>
+        <h1>Landing Page dev</h1>
       </React.Fragment>
     );
 };
