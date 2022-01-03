fc44129c32c488e9e97c6b47971afc6e7b39101f
syROCKS <sameer.yadav.6502@gmail.com>
2022-01-03 21:13:52 +0530

    Navbar design init and Router fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,21 @@ import Landing from "./components/main/Landing";
 import Members from "./components/main/Members";
 import Gallery from "./components/main/Gallery";
 import Events from "./components/main/Events";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/members" element={<Members />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/members" element={<Members />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
       <Footer />
-    </div>
+    </>
   );
 }
 
--- src/components/shared/Navbar/index.jsx
+++ src/components/shared/Navbar/index.jsx
@@ -1,11 +1,62 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 //import'./style.css';
 
 const Navbar = (props) => {
   return (
     <React.Fragment>
-      <h1>Navbar</h1>
+      <nav className="h-20 bg-red-300 w-screen px-4 py-2">
+        <div className="bg-green-300 flex w-full h-full justify-start items-center">
+          <div className="bg-blue-300 flex justify-start items-center leading-none">
+            <Link className="text-5xl cursor-pointer" to="/">
+              Logo
+            </Link>
+          </div>
+          <div className="bg-yellow-300 flex justify-end items-center leading-none w-full">
+            <div className="p-4">
+              <Link
+                className="py-3 px-5 bg-yellow-300 rounded-xl text-xl hover:bg-yellow-500 transition duration-300 ease-in-out flex items-center"
+                to="/home"
+              >
+                Home
+              </Link>
+            </div>
+            <div className="p-4">
+              <Link
+                className="py-3 px-5 bg-yellow-300 rounded-xl text-xl hover:bg-yellow-500 transition duration-300 ease-in-out flex items-center"
+                to="/events"
+              >
+                Events
+              </Link>
+            </div>
+            <div className="p-4">
+              <Link
+                className="py-3 px-5 bg-yellow-300 rounded-xl text-xl hover:bg-yellow-500 transition duration-300 ease-in-out flex items-center"
+                to="/gallery"
+              >
+                Gallery
+              </Link>
+            </div>
+            <div className="p-4">
+              <Link
+                className="py-3 px-5 bg-yellow-300 rounded-xl text-xl hover:bg-yellow-500 transition duration-300 ease-in-out flex items-center"
+                to="/members"
+              >
+                Members
+              </Link>
+            </div>
+            <div className="p-4">
+              <Link
+                className="py-3 px-5 bg-black text-yellow-300 border-2 border-black rounded-full text-xl hover:bg-yellow-300 hover:text-black transition duration-300 ease-in-out flex items-center"
+                to="/members"
+              >
+                Sign IN
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
     </React.Fragment>
   );
 };
